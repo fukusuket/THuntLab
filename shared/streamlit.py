ca885b490f59880fb214d7b6864c66274a7bbbc3
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-09-10 07:43:22 +0900

    Fix formatting of executed search queries display

--- shared/streamlit.py
+++ shared/streamlit.py
@@ -52,7 +52,7 @@ with tab1:
             st.info("Please create /shared/ibh_hunt_*.csv with hunt.py")
 
     with col2:
-        st.markdown(f"Executed Search Queries(")
+        st.markdown(f"Executed Search Queries")
         if not combined_df.empty:
             st.info(f"{len(combined_df)} queries were executed.")
             st.dataframe(combined_df, use_container_width=True, hide_index=True, height=200)
